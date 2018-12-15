bcb056718c4de0a1d342ed7bfcff04e7daf79dcd
Piotr Jaromin <piotr.jaromin@performgroup.com>
2018-12-15 11:30:47 +0100

    removed redundant state

--- ec2-remote-state/backend.tf
+++ ec2-remote-state/backend.tf
@@ -7,10 +7,3 @@ terraform {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-pj-test-bucket" # name of bucket in which we want to store data
-    key    = "ec2-remote-state-test" # this is path were state will be stored inside bucket
-    region = "us-east-1" # region in which bucket exists does NOT need to be the same as in provider
-  }
-}
